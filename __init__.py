a32a90434e5bf8b2c0f8447c4eda1ac34cacd416
unclemusclez <unclemusclez@gmail.com>
2025-06-06 05:14:44 -0400

    directory structure

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from scryfall_sdk import ScryfallSDK
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='scryfall-sdk',
     version='0.0.1',
-    packages=[''],
+    packages=['scryfall'],
     install_requires=[
         'requests',
     ],
@@ -15,7 +15,7 @@ setup(
     url='https://github.com/unclemusclez/scryfall-sdk-python',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        'License :: GPL-3.0-or-later',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
